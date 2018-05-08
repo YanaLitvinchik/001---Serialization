0b9d40a4ee8818ba0d86b8ce401be99f8315f999
Yana <litvinchikyana@ukr.net>
2018-05-08 16:56:51 +0300

    Add project files.

---
+++ ConsoleApplication1/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Info
+    {
+        public int Frequency { get; set; }
+        public string Symbol { get; set; }
+        //info small
+        //info big
+
+    }
+}
---
+++ ConsoleApplication1/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public struct MyStruct : IComparable
+    {
+        public int Frequency { get; set; }
+        public char Letter { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return (int)obj > Frequency ? 1 : 0;
+        }
+    }
+
+    class Node
+    {
+        List<MyStruct> m = new List<MyStruct>();
+        public void Count(string s)
+        {
+            var unique = s.Distinct();
+            foreach (var item in unique)
+            {
+                m.Add(new MyStruct()
+                {
+                    Letter = item,
+                    Frequency = s.Where(x => x == item).Count()
+                });            
+            }
+            m.Sort((b, a) => a.Frequency - b.Frequency);
+            foreach (var item in m)
+            {
+                 Console.WriteLine($"{item.Letter}----{item.Frequency}");
+            }
+        }
+
+    }
+}
